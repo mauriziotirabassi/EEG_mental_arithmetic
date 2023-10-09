b223a3d06a042646151cdf3ac38df0e8121c0a22
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-10-09 22:45:14 +0200

    Data import prototype
    Co-Authored-By: Mattia Pezzano <145902573+M1551N6N0@users.noreply.github.com>

--- untitled.m
+++ untitled.m
@@ -1,1 +1,14 @@
-help load
+clear all
+
+%TODO: implement matrices ds
+
+%Importing the rest data
+for i = 1:6
+    s(i) = load(strcat('Documentation\Data\Subject0', num2str(i), '_1.mat'));
+end
+
+%Importing the activity data
+for i = 1:6
+    t(i) = load(strcat('Documentation\Data\Subject0', num2str(i), '_2.mat'));
+end
+
