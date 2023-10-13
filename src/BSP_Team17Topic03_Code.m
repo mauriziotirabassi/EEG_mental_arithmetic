567748105710b1e026fd558b9a80695d1ac4e5cb
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-10-13 20:29:23 +0200

    New PSD visualizer + time series trim

--- src/BSP_Team17Topic03_Code.m
+++ src/BSP_Team17Topic03_Code.m
@@ -2,27 +2,29 @@ clear variables; close all; clc
 
 %Setting the path to the resources depending on the current OS
 if isunix
-    path = "/Users/mattiapezzano/Documents/GitHub/proj-bsp-2023/Data/";
+    path = '/Users/mattiapezzano/Documents/GitHub/proj-bsp-2023/Data/';
 else
-    path = "Data\";
+    path = 'Data\';
 end
 
 %Loading the data files into a dir struct
-myFiles = dir(strcat(path, "*.mat"));
+myFiles = dir(strcat(path, '*.mat'));
 
 %Loading the file containing the specifics about the EEG
-EEG = load(strcat(path, "chanlocs.mat"));
+%TODO: clean
+tmp = load(strcat(path, 'chanlocs.mat'));
+chanlocs = tmp.chanlocs;
 
 %Iterating through the activity data (skipping the first element)
-for i = (length(myFiles) - 1)/2:-1:1
-    filePath = strcat(path, myFiles(i * 2 - 1).name); %Odd spacing w/ given data
-    myWorkData(i) = load(filePath);
+for tmp = (length(myFiles) - 1)/2:-1:1
+    filePath = strcat(path, myFiles(tmp * 2 - 1).name); %Odd spacing w/ given data
+    myWorkData(tmp) = load(filePath);
 end
 
 %Iterating through the rest data
-for j = (length(myFiles) - 1)/2:-1:1
-    filePath = strcat(path, myFiles(j * 2).name); %Even spacing w/ given data
-    myRestData(j) = load(filePath);
+for tmp = (length(myFiles) - 1)/2:-1:1
+    filePath = strcat(path, myFiles(tmp * 2).name); %Even spacing w/ given data
+    myRestData(tmp) = load(filePath);
 end
 
 %Expliciting the struct attributes in order to iterate through them
@@ -33,13 +35,19 @@ electrodes = ["C3","C4","CZ","F3","F4","F7","F8","FP1","FP2","FZ","O1","O2","P3"
 Fs = 500;
 % t = (1:30*Fs)/Fs;
 
-%% Plotting the location of the electrodes in 3D
-plot3([EEG.chanlocs.X], [EEG.chanlocs.Y], [EEG.chanlocs.Z], 'ko', 'MarkerFaceColor','k');
+%Defining the frequency scale
+hz = linspace(0, Fs/2, floor(Fs/2) + 1);
 
+%Expliciting the number of samples to ignore
+sigErr = 1000;
+
+%% Plotting the location of the electrodes in 3D
+plot3([chanlocs.X], [chanlocs.Y], [chanlocs.Z], 'ko', 'MarkerFaceColor','k');
+
+%Labelling the electrodes
 hold on
-for i = 1:length([EEG.chanlocs.X])
-    %TODO: correct locations
-    text(EEG.chanlocs(i).X + 3, EEG.chanlocs(i).Y, EEG.chanlocs(i).Z, EEG.chanlocs(i).labels)
+for tmp = 1:length(electrodes)
+    text(chanlocs(tmp).X + 3, chanlocs(tmp).Y, chanlocs(tmp).Z, chanlocs(tmp).labels)
 end
 
 xlabel('X'), ylabel('Y'), zlabel('Z')
@@ -48,42 +56,64 @@ axis square
 
 %% Plotting the location of the electrodes in 2D
 
+%TODO: ask if it's ok to use ext sources
+%topoplotIndie(, chanlocs, 'electrodes', 'numbers');
 
-% %TODO:  Undefined function 'finputcheck' for input arguments of type 'cell'.
-% topoplot(zeros(length(electrodes), 1), EEG.chanlocs, 'electrodes', 'numbers')
-
-% for electrode = length(electrodes):-1:1
-%     figure
-%     [handle,Zi,grid,Xi,Yi] = topoplot([chanLocs.chanlocs(i).X], [chanLocs.chanlocs(i).Y], chanLocs.chanlocs(i));
-%     plot(grid);
-% end
+title('2D topographical map');
 
 %% PSD
 
-% Plotting the differences in the PSD fucntion between the rest and 
-% activity state for each person for each electrode
+%Plotting the differences in PSD between working and resting conditions for
+%each subject
 for signal = (length(myFiles) - 1)/2:-1:1 %Same dimension between ds
-    figure;
+    
+    %Computing the PSD with the Welch method for every electrode in both
+    %working and resting conditions
     for electrode = length(electrodes):-1:1
 
         %Expliciting the signals in time domain
         timeWorkSig = myWorkData(signal).(electrodes(electrode));
         timeRestSig = myRestData(signal).(electrodes(electrode));
 
-        %Expliciting the signals in the frequency domain using the Welch
-        %method as specified in the reference paper
-        freqWorkSig = pwelch(timeWorkSig, hamming(Fs*10), Fs*0.1, Fs);
-        freqRestSig = pwelch(timeRestSig, hamming(Fs*10), Fs*0.1, Fs);
+        %Trimming the time series to get rid of signal error
+        timeWorkSig = timeWorkSig(1:length(timeWorkSig) - sigErr);
+        timeRestSig = timeRestSig(1:length(timeRestSig) - sigErr);
 
-        %Plotting the overlap between the two power spectrum densities
-        subplot(5, 4, electrode);
-        plot(freqWorkSig); xlim([0 70]);
-        hold on
-        plot(freqRestSig); xlim([0 70]);
+        %Computing the PSD using the Welch method (specifics in references)
+        freqWorkSig(electrode, :) = pwelch(timeWorkSig, hamming(Fs*10), Fs*0.1, Fs);
+        freqRestSig(electrode, :) = pwelch(timeRestSig, hamming(Fs*10), Fs*0.1, Fs);
 
-        %TODO: fix title: keep info about name of file
-        title(string(electrodes(electrode)));
     end
+
+    %TODO: decide how to sort the electrodes
+    [~,sortXidx] = sort([chanlocs.X]);
+
+    figure
+
+    %Plotting the PSDs for working conditions
+    subplot(2, 1, 1)
+    imagesc(hz,[], freqWorkSig(sortXidx, 1:length(hz)));
+    set(gca, 'xlim', [0 70], 'clim', [0 10]); %Focusing on the 0-70Hz range
+    colorbar
+
+    xlabel('Frequency (Hz)'), ylabel('Electrode')
+    title(strcat('Subject ', int2str(signal), ' work'))
+
+    %Plotting the PSDs for resting conditions
+    subplot(2, 1, 2)
+    imagesc(hz,[], freqRestSig(sortXidx, 1:length(hz)));
+    set(gca, 'xlim', [0 70], 'clim', [0 10]); %Focusing on the 0-70Hz range
+    colorbar
+
+    xlabel('Frequency (Hz)'), ylabel('Electrode')
+    title(strcat('Subject ', int2str(signal), ' rest'))
+
+    %TODO: choose colorbar range relatively to the normalization of the
+    %fourier transform(?)
+        
+    %TODO: here plotting the actual differences once having calculated all
+    %the psds
+
 end
 
 %% Detrended Fluctuation Analysis
