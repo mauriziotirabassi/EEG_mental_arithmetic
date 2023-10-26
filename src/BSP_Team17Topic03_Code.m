4a19313955bd3e6ecb75dbf3e783ee7cfb7607d6
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-10-26 18:22:41 +0200

    Boxplot
    Co-Authored-By: Mattia Pezzano <145902573+M1551N6N0@users.noreply.github.com>

--- src/BSP_Team17Topic03_Code.m
+++ src/BSP_Team17Topic03_Code.m
@@ -37,20 +37,6 @@ for i = 1:chanNum %TODO: length chanlocs - 2 redundant
     electrodes(i) = upper(chanlocs(i).labels);
 end
 
-%% PLOTTING 3D ELECTRODE LOCATION
-figure
-plot3([chanlocs.X], [chanlocs.Y], [chanlocs.Z], 'ko', 'MarkerFaceColor','k');
-
-% Labelling the electrodes
-hold on
-for tmp = 1:length(electrodes)
-    text(chanlocs(tmp).X + 3, chanlocs(tmp).Y, chanlocs(tmp).Z, chanlocs(tmp).labels)
-end
-
-xlabel('X'), ylabel('Y'), zlabel('Z')
-title('Electrode Positions')
-axis square
-
 %% DATA PREPROCESSING
 
 % Preallocating memory for the data structures containing the time series
@@ -58,7 +44,6 @@ axis square
 workBands = zeros(length(electrodes), length(freqBands), subjNum);
 restBands = zeros(length(electrodes), length(freqBands), subjNum);
 
-figure
 for subj = subjNum:-1:1
 
     % Defining the number of electrode channels belonging to each lobe
@@ -71,13 +56,9 @@ for subj = subjNum:-1:1
         timeWork = myWorkData(subj).(eTag);
         timeRest = myRestData(subj).(eTag);
 
-        % Trimming the time series to get rid of signal error
+        % Trimming the time series to get rid of artifacts
         timeWork = timeWork(1:length(timeWork) - sigErr);
         timeRest = timeRest(1:length(timeRest) - sigErr);
-
-        %Detrending the time series to remove the constant contribution
-        timeWork = detrend(timeWork);
-        timeRest = detrend(timeRest);
 
         % Selecting a limited window to approximate for stationarity
         % (dimW seconds centered in the signal median)
@@ -111,30 +92,25 @@ for subj = subjNum:-1:1
 
             % Computing the mean frequency for every band for every subject
             psdBand(electrode, freq, subj) = trapz(intervalX, intervalY);
-
         end
 
-        %TODO: decide if it's more convenient having it isolated inside
-        %another section
-
-        % Dividing electrode channels among lobes for each subject
+        % Dividing band channels among lobes for each subject
         if(startsWith(eTag, 'F'))
-            frntPsd(frntNum, :, subj) = psdRatio(electrode, :, subj);
+            frntPsd(frntNum, :, subj) = psdBand(electrode, :, subj);
             frntNum = frntNum - 1;
         elseif(startsWith(eTag, 'P') || startsWith(eTag, 'C'))
-            prtPsd(prtNum, :, subj) = psdRatio(electrode, :, subj);
+            prtPsd(prtNum, :, subj) = psdBand(electrode, :, subj);
             prtNum = prtNum - 1;
         elseif(startsWith(eTag, 'O'))
-            occPsd(occNum, :, subj) = psdRatio(electrode, :, subj);
+            occPsd(occNum, :, subj) = psdBand(electrode, :, subj);
             occNum = occNum - 1;
         elseif(eTag == "T4" || eTag == "T6")
-            tmpdxPsd(tmpdxNum, :, subj) = psdRatio(electrode, :, subj);
+            tmpdxPsd(tmpdxNum, :, subj) = psdBand(electrode, :, subj);
             tmpdxNum = tmpdxNum - 1;
         elseif(eTag == "T3" || eTag == "T5")
-            tmpsxPsd(tmpsxNum, :, subj) = psdRatio(electrode, :, subj);
+            tmpsxPsd(tmpsxNum, :, subj) = psdBand(electrode, :, subj);
             tmpsxNum = tmpsxNum - 1;
         end
-
     end
 
     % Computing the average frequency for each lobe for each subject
@@ -144,49 +120,67 @@ for subj = subjNum:-1:1
     tmpdxPsdAvg(subj, :) = mean(tmpdxPsd(:, :, subj));
     tmpsxPsdAvg(subj, :) = mean(tmpsxPsd(:, :, subj));
 
-    % Denormalizing the frequency range (implicitly normalized by the pwelch
-    % function)
-    %TODO: if not plotting can define outside of the outer for loop
-    hz = hz*Fs/2/pi;
-
-    %TODO: decide if graphs are relevant (include process decision making
-    %in the paper)
-
-    % Sorting the electrodes relatively to their X coordinate
-    % (from the occipital lobe to the frontal lobe)
-    [~, sortXidx] = sort([chanlocs.X]);
-
-    % Plotting the ratio normalized psd
-    subplot(3, 2, subj)
-    imagesc(hz, [], psdRatio(sortXidx, 1:length(hz), subj));
-
-    % Deciding the color range knowing a priori the frequency range
-    set(gca, 'xlim', [0 70], 'clim', [-30 30]); %Focusing on the 0-70Hz range
-    colorbar
-    xlabel('Frequency (Hz)'), ylabel('F <-- --> O')
-    title(strcat('Subj', int2str(subj), ' Work'))
-    
 end
 
-%% PLOTTING AVERAGED TOPOGRAPHICAL MAPS
+% Denormalizing the frequency range (implicitly normalized by the pwelch
+% function)
+hz = hz*Fs/2/pi;
 
+%% PLOTTING 3D ELECTRODE LOCATION
+figure
+plot3([chanlocs.X], [chanlocs.Y], [chanlocs.Z], 'ko', 'MarkerFaceColor','k');
+
+% Labelling the electrodes
+hold on
+for tmp = 1:length(electrodes)
+    text(chanlocs(tmp).X + 3, chanlocs(tmp).Y, chanlocs(tmp).Z, chanlocs(tmp).labels)
+end
+
+xlabel('X'), ylabel('Y'), zlabel('Z')
+title('Electrode Positions')
+axis square
+
+%% PLOTTING AVERAGED TOPOGRAPHICAL MAPS
 %Computing the average over subjects
 avgBandSig = mean(psdBand, 3);
 figure
 for freq = 1:(length(freqBands) - 1)
     subplot(2, 3, freq)
-    topoplot(avgBandSig(:, freq), chanlocs, 'electrodes', 'labels')
+    topoplot(avgBandSig(:, freq), chanlocs, 'electrodes', 'labels', 'maplimits', [-20 20])
     colormap parula
+    colorbar
     title(['(' int2str(freqBands(freq)) '-' int2str(freqBands(freq + 1)) ')Hz'])
 end
 
-%% COHERENCE BETWEEN CHANNELS
+%% BOXPLOT AVERAGE POWER OVER LOBES
+%TODO: comment
+figure
+subplot(2, 3, 1)
+boxplot(frntPsdAvg, 'labels', {'δ', 'θ', 'α', 'β1', 'β2', 'γ'})
+title('Frontal'), ylabel('Average Power [dB]')
 
-for k=subjNum:-1:1
-    for i= 1:length(electrodes)
-        for j= 1:length(electrodes)
-            RestMSC(i,j,:,k)=mscohere(RestRecord(i,:,k),RestRecord(j,:,k),hamming(Fs*10), Fs*0.1, Fs*10);
-            WorkMSC(i,j,:,k)=mscohere(WorkRecord(i,:,k),WorkRecord(j,:,k),hamming(Fs*10), Fs*0.1, Fs*10);
+subplot(2, 3, 2)
+boxplot(prtPsdAvg, 'labels', {'δ', 'θ', 'α', 'β1', 'β2', 'γ'})
+title('Parietal'), ylabel('Average Power [dB]')
+
+subplot(2, 3, 3)
+boxplot(occPsdAvg, 'labels', {'δ', 'θ', 'α', 'β1', 'β2', 'γ'})
+title('Occipital'), ylabel('Average Power [dB]')
+
+subplot(2, 3, 4)
+boxplot(tmpdxPsdAvg, 'labels', {'δ', 'θ', 'α', 'β1', 'β2', 'γ'})
+title('Right Temporal'), ylabel('Average Power [dB]')
+
+subplot(2, 3, 5)
+boxplot(tmpsxPsdAvg, 'labels', {'δ', 'θ', 'α', 'β1', 'β2', 'γ'})
+title('Left Temporal'), ylabel('Average Power [dB]')
+
+%% COHERENCE BETWEEN CHANNELS
+for k = subjNum:-1:1
+    for i = 1:length(electrodes)
+        for j = 1:i
+            RestMSC(i,j,:,k)=mscohere(restRecord(i,:,k), restRecord(j,:,k),hamming(Fs*10), Fs*0.1, Fs*10);
+            WorkMSC(i,j,:,k)=mscohere(workRecord(i,:,k), workRecord(j,:,k),hamming(Fs*10), Fs*0.1, Fs*10);
         end
     end
 end
